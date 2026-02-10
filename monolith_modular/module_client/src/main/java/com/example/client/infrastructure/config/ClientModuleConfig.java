0097aa4814048c40d1bd72311db5701c4b2ba9c8
Wanderson Lira <wandersonlira.dev1@gmail.com>
2026-02-10 20:57:45 -0300

    :sparkles: feat: Ajuste no caminho da pasta contendo as corretas anotações do JPA e Entity

--- monolith_modular/module_client/src/main/java/com/example/client/infrastructure/config/ClientModuleConfig.java
+++ monolith_modular/module_client/src/main/java/com/example/client/infrastructure/config/ClientModuleConfig.java
@@ -1,12 +1,12 @@
 package com.example.client.infrastructure.config;
 
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @ComponentScan(basePackages = "com.example.client")
-@EntityScan(basePackages = "com.example.client.infrastructure.persistence")
-@EnableJpaRepositories(basePackages = "com.example.client.infrastructure.persistence")
+@EntityScan(basePackages = "com.example.client.infrastructure.persistence.entities")
+@EnableJpaRepositories(basePackages = "com.example.client.infrastructure.persistence.repositories")
 public class ClientModuleConfig { }
