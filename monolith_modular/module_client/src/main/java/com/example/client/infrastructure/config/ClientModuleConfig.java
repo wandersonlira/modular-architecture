04bd4725bdd0eab83004306a56dd9e4400a47f3c
Wanderson Lira <wandersonlira.dev1@gmail.com>
2026-05-01 10:56:28 -0300

    :sparkles: feat: Configuração dos @ComponentScan das packages 'presentation e infrastructure'

--- monolith_modular/module_client/src/main/java/com/example/client/infrastructure/config/ClientModuleConfig.java
+++ monolith_modular/module_client/src/main/java/com/example/client/infrastructure/config/ClientModuleConfig.java
@@ -3,10 +3,12 @@ package com.example.client.infrastructure.config;
 
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@ComponentScan(basePackages = "com.example.client")
+@Configuration
+@ComponentScan(basePackages = {"com.example.client.presentation", "com.example.client.infrastructure"})
 @EntityScan(basePackages = "com.example.client.infrastructure.persistence.entities")
 @EnableJpaRepositories(basePackages = "com.example.client.infrastructure.persistence.repositories")
 public class ClientModuleConfig { }
